1087dffc19fd41b9e978e51960f5a568f112b955
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-08-09 18:38:19 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <!-- heading -->
+     <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo">
+      <h1>{{ taskStore.name }}</h1>
+    </header>
+
+    <!-- task list -->
+     <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task">
+        <TaskDetails :task="task"/>
+      </div>
+     </div>
+  </main>
+</template>
+
+<script>
+import { useTaskStore } from './stores/TaskStore';
+import TaskDetails from './components/TaskDetails.vue';
+
+export default {
+  components: { TaskDetails },
+  setup () {
+    const taskStore = useTaskStore();
+
+    return {taskStore};
+  }
+};
+</script>
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="task">
+        <h1>{{ task.title }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['task']
+    }
+</script>
